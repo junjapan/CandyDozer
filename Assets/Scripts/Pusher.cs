e031658720d98b0bd12495410829fed4baf675c7
jun <watanabejapan2trap@gmail.com>
2022-01-28 13:51:38 +0900

    p178まで作成

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPosition;
+
+    public float amplitude;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //localpositionは親からのポジション
+        startPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mathf.Sinはサイン関数
+        float z = amplitude * Mathf.Sin(Time.time * speed);
+        transform.localPosition = startPosition + new Vector3(0, 0, z);
+    }
+}
